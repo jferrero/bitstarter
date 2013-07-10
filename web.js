a47b894d61cb23e179654f813ed07bde44e299d0
Ubuntu <ubuntu@ip-172-31-37-184.us-west-2.compute.internal>
2013-07-10 13:28:00 +0000

    master: segundo commit de evlauacion con lectura de buffer

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+var contenido= fs.readFileSync('index.html');
+response.send(contenido.toString());
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
